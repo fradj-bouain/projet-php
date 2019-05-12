149723336d7115752cfe3aabbf7501acdbf81339
fradj-bouain <fredejguide@gmail.com>
2019-05-12 14:55:06 +0200

    delete

--- employe/logout.php
+++
@@ -1,8 +0,0 @@
-<?php
-// Démarrer une session
-session_start();
-// Détruire toutes les données de la session courant
-session_destroy();
-// Redirection vers la page de connexion
-header("Location: login/index.php");
-
--- employe/supp-sql.php
+++
@@ -1,9 +0,0 @@
-<?php
-// Récupérer l'id à supprimer
-$id = $_GET['id'];
-// Connexion à la bdd
-include('bd.php');
-// Supprimer l'enregistrement
-$cnx->exec("DELETE FROM formulaire where id=$id");
-// Redirection à la page principale
-header("Location: employes.php");
